8fc0074cce05e097ab292238aa1edcd01a18a566
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-25 14:57:27 -0300

    form component

---
+++ src/Components/Form/Input.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = ({ label, type, name }) => {
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={name}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        className={styles.input}
+      ></input>
+      <p className={styles.error}>Error</p>
+    </div>
+  );
+};
+
+export default Input;
