032b5c3a2c3f14612fd50cb11238dbf0621aadca
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-25 11:45:32 -0300

    estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 
 const App = (props) => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
