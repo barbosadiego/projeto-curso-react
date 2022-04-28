eaa41c53df770bd07b6b9ff7309f6c9607bef8c7
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-28 09:56:18 -0300

    image skeleton

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import styles from './FeedPhotosItem.module.css'
+import React from 'react';
+import Image from '../Help/Image';
+import styles from './FeedPhotosItem.module.css';
 
-const FeedPhotosItem = ({photo, setModalPhoto}) => {
-
-  function handleClick(){
-    setModalPhoto(photo)
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
   }
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
-  )
-}
+  );
+};
 
-export default FeedPhotosItem
+export default FeedPhotosItem;
