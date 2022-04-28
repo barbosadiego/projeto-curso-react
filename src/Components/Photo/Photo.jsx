d6922c7eb88d9d02cebc7911ef7f684529af3b95
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-28 14:42:10 -0300

    foto pagina

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { PHOTO_GET2 } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Erro from '../Help/Erro';
+import Loading from '../Help/Loading';
+import PhotoContent from './PhotoContent';
 
 const Photo = (props) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
 
-export default Photo
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET2(id);
+    request(url, options);
+  }, [id, request]);
+
+  if (error) return <Erro error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent single={true} data={data} />
+      </section>
+    );
+  else return null;
+};
+
+export default Photo;
